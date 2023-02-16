c3d35432e17f67f93df1f5cf78ed3eeccfcbd4d9
yaelBrown <yaelrbrown@gmail.com>
2023-02-16 10:57:31 -0600

    feat: completed quotes api

---
+++ YaelQuotesCS/Controllers/QuotesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using System.Web;
+using System.Web.Http;
+using YaelQuotesCS.Models;
+using Microsoft.AspNetCore.Http.Internal;
+using System.Security.Cryptography.X509Certificates;
+
+namespace YaelQuotesCS.Controllers
+{
+    [Produces("application/json")]
+    public class QuotesController : ApiController
+    {
+        public string Get()
+        {
+            StreamReader r = new StreamReader(HttpContext.Current.Server.MapPath("/Content/JSON/quotes.json"));
+            string json = r.ReadToEnd();
+
+            Console.WriteLine(json);
+            QuotesModel quotes = JsonConvert.DeserializeObject<QuotesModel>(json);
+
+            Random rnd = new Random();
+            int idx = rnd.Next(quotes.q.Count);
+
+            return quotes.q[idx];
+        }
+    }
+}
---
+++ YaelQuotesCS/Models/QuotesModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace YaelQuotesCS.Models
+{
+    public class QuotesModel
+    {
+        public List<string> q = new List<string>();
+    }
+}
